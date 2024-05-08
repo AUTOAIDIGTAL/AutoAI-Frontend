0a8eb35c7281b911818fe00e7d4788666511f9ec
tayyab-khilji <tayyab.khilji@devsparklabs.com>
2024-05-08 13:54:34 +0500

    minor

--- front-end-autoao/src/hooks/auth/useLogin.js
+++ front-end-autoao/src/hooks/auth/useLogin.js
@@ -3,11 +3,15 @@ import Cookies from "js-cookie";
 
 export const useLogin = () => {
 	const login = async (email, password) => {
-		const user = await authService.login(email, password);
-		if (user?.data) {
-			Cookies.set("currentUser", JSON.stringify(user?.data));
+		try {
+			const user = await authService.login(email, password);
+			if (user?.data) {
+				Cookies.set("currentUser", JSON.stringify(user?.data));
+			}
+			return user
+		} catch (error) {
+			return error
 		}
-		return user
 	};
 
 	return { login };
