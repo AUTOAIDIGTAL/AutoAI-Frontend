a2806695c5ef4fc95b227c2b6f44df27a0447143
tayyab-khilji <Tayyabaqeel222@gmail.com>
2024-05-24 16:44:57 +0500

    Update index.js

--- front-end-autoao/src/services/index.js
+++ front-end-autoao/src/services/index.js
@@ -2,6 +2,5 @@ import { AuthService } from "./auth.service";
 import { APIService } from "./api.service";
 
 let BASE_URL = process.env.NEXT_PUBLIC_API_URL
-
 export const authService = new AuthService(BASE_URL);
 export const apiService = new APIService(BASE_URL);
