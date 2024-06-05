82f3fc4b6bd0618180d60d85db0eb5205ea22db9
tayyab-khilji <tayyab.khilji@devsparklabs.com>
2024-06-05 14:07:03 +0500

    minor

--- front-end-autoao/src/app/(pages)/(dashboard)/scheduled-tasks/explore/kanban-head/page.js
+++ front-end-autoao/src/app/(pages)/(dashboard)/scheduled-tasks/explore/kanban-head/page.js
@@ -1,22 +1,23 @@
+"use client"
 import React from "react";
 import WorkOrderModal from "../(work-required-modal)/page";
 import DropdownWithForm from "../(work-filter-modal-form)/page";
 
 const KanbanHead = () => {
-  return (
-    <div>
-      <div className="d-flex justify-content-between align-items-center">
-        <div  className="d-flex align-items-center gap-3">
-          <div className="fs-5 fw-medium">Tasks View</div>
-        </div>
-        <div className="d-flex align-items-center gap-3">
-          <DropdownWithForm />
-          <WorkOrderModal />
-        </div>
-      </div>
-      <div className="flex-1 pt-3">{/* <PaginationUi /> */}</div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="d-flex justify-content-between align-items-center">
+				<div className="d-flex align-items-center gap-3">
+					<div className="fs-5 fw-medium">Tasks View</div>
+				</div>
+				<div className="d-flex align-items-center gap-3">
+					<DropdownWithForm />
+					<WorkOrderModal />
+				</div>
+			</div>
+			<div className="flex-1 pt-3">{/* <PaginationUi /> */}</div>
+		</div>
+	);
 };
 
 export default KanbanHead;
