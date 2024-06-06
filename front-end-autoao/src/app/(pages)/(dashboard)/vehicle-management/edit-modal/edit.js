9e49d88cf5767327c297ff66a3d2d73ff049b9cb
tayyab-khilji <tayyab.khilji@devsparklabs.com>
2024-06-06 21:57:06 +0500

    minor

--- front-end-autoao/src/app/(pages)/(dashboard)/vehicle-management/edit-modal/edit.js
+++ front-end-autoao/src/app/(pages)/(dashboard)/vehicle-management/edit-modal/edit.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useSyncExternalStore } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import { Row, Col, Dropdown } from 'react-bootstrap';
 import { constants } from '../../garage-management/constant';
 import { apiService } from '@/services';
+import Multiselect from 'multiselect-react-dropdown';
 
 const EditModal = ({ vehicle, onVehicleUpdated }) => {
 
@@ -24,7 +25,8 @@ const EditModal = ({ vehicle, onVehicleUpdated }) => {
 	const [selectedFiles, setSelectedFiles] = useState(null);
 	const [color, setColor] = useState(null);
 	const [fuelType, setFuelType] = useState(null);
-
+	const [options, setOptions] = useState([]);
+	const clientRef = useRef(null);
 	useEffect(() => {
 		if (vehicle) {
 			setVinNumber(vehicle.vinNumber);
@@ -81,6 +83,8 @@ const EditModal = ({ vehicle, onVehicleUpdated }) => {
 			return;
 		}
 
+		debugger;
+
 		const data = new FormData();
 		data.append('vinNumber', vinNumber);
 		data.append('regPlate', regNumber);
@@ -103,6 +107,19 @@ const EditModal = ({ vehicle, onVehicleUpdated }) => {
 			setShow(false);
 		}
 	};
+
+	const handleSearch = async (val) => {
+		try {
+			if (val?.length > 3) {
+				const searchResult = await apiService.get(
+					`${constants.searchCustomer}?search=${val}`
+				);
+				setOptions(searchResult);
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
 	return (
 		<>
@@ -190,8 +207,34 @@ const EditModal = ({ vehicle, onVehicleUpdated }) => {
 							<Col sm={12} xl={5}>
 								<Form.Group className="mb-3" controlId="formClient">
 									<Form.Label>Client</Form.Label>
-
 									<div className="position-relative">
+										<Multiselect
+											isObject={true}
+											options={options}
+											displayValue="name"
+											className="bg-gray-100 rounded-ai"
+											onSearch={(val) => handleSearch(val)}
+											ref={clientRef}
+											selectionLimit={1}
+											selectedValues={[client]}
+											onSelect={(client) => { setClient(client[0]) }} // 
+										/>
+										<span className="position-absolute top-50 end-15 translate-middle">
+											<svg
+												width={14}
+												height={14}
+												viewBox="0 0 14 14"
+												fill="none 206 5.0413 12.0001 6.49999 12.0001Z"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<path
+													d="M11.436 10.7301L13.389 12.6821C13.4801 12.7764 13.5305 12.9027 13.5293 13.0338C13.5282 13.1649 13.4756 13.2903 13.3829 13.383C13.2902 13.4757 13.1648 13.5283 13.0337 13.5294C12.9026 13.5306 12.7763 13.4802 12.682 13.3891L10.729 11.4361C9.45275 12.5295 7.8026 13.0862 6.1248 12.9892C4.44701 12.8922 2.87199 12.1491 1.73024 10.9159C0.588492 9.6827 -0.0312305 8.05519 0.00111371 6.3749C0.0334579 4.69462 0.715354 3.09217 1.90372 1.90381C3.09208 0.715446 4.69453 0.0335495 6.37481 0.00120526C8.0551 -0.031139 9.6826 0.588583 10.9158 1.73033C12.149 2.87208 12.8921 4.4471 12.9891 6.1249C13.0861 7.80269 12.5294 9.45284 11.436 10.7291V10.7301ZM6.49999 12.0001C7.95868 12.0001 9.35763 11.4206 10.3891 10.3892C11.4205 9.35772 12 7.95877 12 6.50008C12 5.04139 11.4205 3.64244 10.3891 2.61099C9.35763 1.57954 7.95868 1.00008 6.49999 1.00008C5.0413 1.00008 3.64235 1.57954 2.6109 2.61099C1.57945 3.64244 0.99999 5.04139 0.99999 6.50008C0.99999 7.95877 1.57945 9.35772 2.6109 10.3892C3.64235 11.4206 5.0413 12.0001 6.49999 12.0001Z"
+													fill="#1A202C"
+												/>
+											</svg>
+										</span>
+									</div>
+									{/* <div className="position-relative">
 										<Form.Control
 											type="text"
 											placeholder="Search Client"
@@ -221,7 +264,7 @@ const EditModal = ({ vehicle, onVehicleUpdated }) => {
 												</Dropdown.Item>
 											))}
 										</Dropdown.Menu>
-									</Dropdown>
+									</Dropdown> */}
 								</Form.Group>
 							</Col>
 						</Row>
